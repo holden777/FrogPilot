a197ff7e4b866d2ada1ba2c58d367f46aa1fd8c0
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-01-20 18:46:48 -0700

    Vision Turn Speed Controller
    Added toggles for "Vision Turn Speed Control" along with aggressiveness for the speed and sensitivity for the curve itself.
    
    Credit goes to Pfeiferj!
    
    https: //github.com/pfeiferj
    Co-Authored-By: Jacob Pfeifer <jacob@pfeifer.dev>

--- common/params.cc
+++ common/params.cc
@@ -241,6 +241,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"Compass", PERSISTENT},
     {"ConditionalExperimental", PERSISTENT},
     {"CurrentRandomEvent", PERSISTENT},
+    {"CurveSensitivity", PERSISTENT},
     {"CustomColors", PERSISTENT},
     {"CustomIcons", PERSISTENT},
     {"CustomPersonalities", PERSISTENT},
@@ -358,6 +359,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"StoppingDistance", PERSISTENT},
     {"TetheringEnabled", PERSISTENT},
     {"TSS2Tune", PERSISTENT},
+    {"TurnAggressiveness", PERSISTENT},
     {"TurnDesires", PERSISTENT},
     {"UnlimitedLength", PERSISTENT},
     {"Updated", PERSISTENT},
@@ -365,6 +367,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"UpdateTime", PERSISTENT},
     {"UseSI", PERSISTENT},
     {"UseVienna", PERSISTENT},
+    {"VisionTurnControl", PERSISTENT},
     {"WheelIcon", PERSISTENT},
 };
 
